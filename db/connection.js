9ae3377e95a39469415f7ede0e38e3ce5831653f
Avijit Samanta <67010969+samantaavijit@users.noreply.github.com>
2022-09-20 07:29:03 +0530

    db connection

--- db/connection.js
+++ db/connection.js
@@ -4,20 +4,16 @@ const databaseName = "tuition";
 const mongoURI = `mongodb://localhost:27017/${databaseName}?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 
 mongoose.connect(mongoURI);
-
 var conn = mongoose.connection;
 
-///////////////database connect////////////////////////
 conn.on("connected", () => {
   console.log("database is connected successfully");
 });
 
-///////////////database disconnect/////////////////////
 conn.on("disconnected", () => {
   console.log("database is disconnected successfully");
 });
 
-///////////////database error//////////////////
 conn.on("error", console.error.bind(console, "connection error:"));
 
 module.exports = conn;

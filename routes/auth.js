08e4cd45aebf174c95015d3a76015b9660f9d0df
Avijit Samanta <67010969+samantaavijit@users.noreply.github.com>
2022-09-27 08:55:55 +0530

    add registration

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,13 @@ Router.post("/login", async (req, res) => {
 
 Router.post(
   "/registration",
-  [check("name", "Name is Required").not().isEmpty()],
+  [
+    check("name", "Name is Required").not().isEmpty(),
+    check("email", "Enter valid email").isEmail().isLowercase(),
+    check("password", "Password must be 6 or more characters").isLength({
+      min: 6,
+    }),
+  ],
   async (req, res) => {
     const errors = validationResult(req);
 
@@ -28,8 +34,36 @@ Router.post(
       // });
     }
 
-    // console.log(req.body);
-    res.send("Welcome to Registration"); // response
+    let { name, email, password } = req.body; // de-structure
+    // let name = req.body.name;
+    // let email=req.body.email
+
+    try {
+      // check whether a user is present or not
+      let findUser = await User.findOne({ email: email });
+      if (findUser) {
+        // existing user
+        return res.status(400).json({ error: "Email id already exist" });
+      }
+      // create new user
+      let newUser = await User.create({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      if (newUser) {
+        return res
+          .status(200)
+          .json({ success: true, message: "User successfully registered" });
+      }
+      return res
+        .status(400)
+        .json({ success: true, error: "Userd register failed" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
